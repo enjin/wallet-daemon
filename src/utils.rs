cd0be6b374ff8ee0b03fed69a94ddb8d65b79ed2
Jay Pavlina <explodingjay@protonmail.com>
2026-04-17 13:34:20 -0300

    add logging and only accept status 200

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
 use crate::global;
 use backon::{ExponentialBuilder, Retryable};
+use reqwest::StatusCode;
+use std::fmt::Debug;
 
 /// Helper function to execute a graphql query
 pub async fn execute_query<T: graphql_client::GraphQLQuery>(
@@ -8,9 +10,10 @@ pub async fn execute_query<T: graphql_client::GraphQLQuery>(
 ) -> Result<T::ResponseData, Box<dyn std::error::Error + Send + Sync>>
 where
     T::Variables: serde::Serialize,
+    T::ResponseData: Debug,
 {
     let query_body = T::build_query(variables);
-    // println!("query_body: {:?}", serde_json::to_string(&query_body));
+    tracing::info!("Request Body: {:?}", serde_json::to_string(&query_body));
     let client = global::graphql_client().await;
 
     let response = if let Some(strategy) = retry_strategy {
@@ -32,8 +35,11 @@ where
             .send()
             .await?
     };
-
-    let response_body: graphql_client::Response<T::ResponseData> = response.json().await?;
-
-    response_body.data.ok_or("no response data".into())
+    if response.status() == StatusCode::OK {
+        let response_body: graphql_client::Response<T::ResponseData> = response.json().await?;
+        tracing::info!("Response Body: {response_body:?}");
+        response_body.data.ok_or("no response data".into())
+    } else {
+        Err(format!("response not ok: {:?}", response).into())
+    }
 }
